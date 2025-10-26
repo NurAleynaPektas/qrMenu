a3d49e327e2474c4a17145e310137ada8f26c9dd
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-10-26 15:00:24 +0300

    setup routing and basic layout with Navbar and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,23 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      </Routes>
       <Footer />
     </>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import s from "./Navbar.module.css"
+import React from "react";
+import s from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    
     <div className={s.navbar}>
       <div className={s.navbarLeft}>
         <h2>QrMenu</h2>
       </div>
-          <div className={s.navbarRight}>
-              <a href="/">Menu</a>
-              <a href="cart">Cart</a>
+      <div className={s.navbarRight}>
+        <Link to="/">Menü</Link>
+        <Link to="/cart">Sepetim</Link>
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
